8e66bf5c490ce1f9cd992fdc2598a2ae7d375530
Artur Santiago <artursantiago@info.ufrn.br>
2021-01-18 21:25:27 -0300

    first commit

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,5 @@
+import style from 'styled-components';
+
+export const Title = style.h1`
+  font-size: 48px;
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, button, input {
+    font: 1.6rem Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 96rem;
+    margin: 0 auto;
+    padding: 4rem 2rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
