9e6d05a69572585202fed0a61d0eb5506b7c481e
Artur Santiago <artursantiago@info.ufrn.br>
2021-01-22 17:06:36 -0300

    styling repos page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Logo do Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/47838241"
+            alt="Artur Santiago"
+          />
+          <div>
+            <strong>artursantiago/pokedex</strong>
+            <p>descrição do projeto</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>64</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <a href="teste">
+          <div>
+            <strong>Isssue lorem ipsum</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
